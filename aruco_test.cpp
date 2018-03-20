526020f1af18d1a1c48e3f0634d3963ebfaf6ddb
Yulong <ywang338@jhu.edu>
2018-03-20 17:23:19 -0400

    First modification: Ofstream the data file.

--- aruco_test.cpp
+++ aruco_test.cpp
@@ -1,33 +1,4 @@
-
-/**
-Copyright 2017 Rafael Muñoz Salinas. All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification, are
-permitted provided that the following conditions are met:
-
-   1. Redistributions of source code must retain the above copyright notice, this list of
-      conditions and the following disclaimer.
-
-   2. Redistributions in binary form must reproduce the above copyright notice, this list
-      of conditions and the following disclaimer in the documentation and/or other materials
-      provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY Rafael Muñoz Salinas ''AS IS'' AND ANY EXPRESS OR IMPLIED
-WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Rafael Muñoz Salinas OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-The views and conclusions contained in the software and documentation are those of the
-authors and should not be interpreted as representing official policies, either expressed
-or implied, of Rafael Muñoz Salinas.
-*/
-
- #include "aruco.h"
+#include "aruco.h"
 #include "cvdrawingutils.h"
 #include <fstream>
 #include <iostream>
@@ -234,7 +205,11 @@ int main(int argc, char** argv)
         char key = 0;
         int index = 0,indexSave=0;
         // capture until press ESC or until the end of the video
-
+		
+		
+		ofstream datafile;
+		datafile.open ("data.txt");
+		
          do
         {
 
@@ -256,10 +231,13 @@ int main(int argc, char** argv)
                 for(auto cand:candidates)
                     Marker(cand,-1).draw(TheInputImageCopy, Scalar(255, 0, 255));
             }
-
+				
+			
+			
             for (unsigned int i = 0; i < TheMarkers.size(); i++)
             {
                 cout << TheMarkers[i] << endl;
+				datafile << TheMarkers[i] << endl;
                 TheMarkers[i].draw(TheInputImageCopy, Scalar(0, 0, 255),2,true);
             }
 
@@ -268,7 +246,6 @@ int main(int argc, char** argv)
                 for (unsigned int i = 0; i < TheMarkers.size(); i++)
                 {
                     CvDrawingUtils::draw3dCube(TheInputImageCopy, TheMarkers[i], TheCameraParameters);
-                    CvDrawingUtils::draw3dAxis(TheInputImageCopy, TheMarkers[i], TheCameraParameters);
                 }
 
             // DONE! Easy, right?
@@ -332,6 +309,7 @@ int main(int argc, char** argv)
     {
         cout << "Exception :" << ex.what() << endl;
     }
+	datafile.close();
 }
 
 
